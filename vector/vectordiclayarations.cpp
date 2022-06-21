a4df5b6c9448f375cb96530caa4200146d509bcf
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2022-06-21 19:51:50 +0600

    Vector_Diclayerations_from_STL

---
+++ vector/vectordiclayarations.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>//this for all header file
+using namespace std;
+
+int main(){
+    int a[4]; //array diclayeration
+    
+    vector<int> v;//vector diclayeration
+    v.push_back ( 2 );//location=v[0]
+    v.push_back ( 10 );//location= v[1]
+    v.push_back ( 18 );//location= v[2]
+    cout<<v[1]<<endl;//output will be vector1 that's mean 10
+    cout<< v.size()<<endl;//it's the size of the vactor
+}
