3326b5132c09f5eff2ea6c491331b5ad7e95c8ad
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2022-08-31 09:32:46 +0600

    luckgo graphacali larger to smaller

---
+++ set/set_with_string/greater<string>_with_set.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include<cmath>
+
+     
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main(){
+        optimize();
+        
+        set<string, greater<string>> s;//it's will print decrsing order
+        s.insert("sahin");
+        s.insert("shaon");
+        s.insert("sohan");
+        s.insert("sohan");
+        s.insert("sahin");
+        s.insert("shaon");
+        for(auto u:s) cout<<u<<" ";//it's will print luckgo graphacali larger to smaller
+        
+        cout<<endl;
+     
+}
