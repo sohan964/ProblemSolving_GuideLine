efa06ed6b8f09fdfcb9b1ce24017f95ebfa2ef3f
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2022-10-26 10:03:42 +0600

    my template

---
+++ template.cpp
@@ -0,0 +1,34 @@
+//  BISMILLAHIR RAHMANIR RAHIM //
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double,double> pdd;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef double dl;
+
+
+#define endl '\n'
+#define F first
+#define S second
+#define PB push_back
+#define PF push_front
+
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main(){
+  optimize();
+   
+   
+    
+}
