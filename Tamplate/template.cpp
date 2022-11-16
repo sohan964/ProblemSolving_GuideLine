10c45acfdde4347a0a06d3659b4db65f9cfb0bb3
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2022-11-16 08:03:46 +0600

    importance mal

--- Tamplate/template.cpp
+++ Tamplate/template.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 //this for policy based DS
+
 #include<ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 typedef tree <long long, null_type, less_equal<long long>,rb_tree_tag,tree_order_statistics_node_update> pbds_set;
+// cout<<*x.find_by_order(1) <<endl; // iterator to k-th largest element
+// cout<<x.order_of_key(-3)<<endl; // number of items in a set that are strictly smaller than the item
+
 // up to this
 
 typedef long long   ll;
